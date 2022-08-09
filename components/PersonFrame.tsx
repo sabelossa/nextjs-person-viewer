5c2bc773a8953c7cd0d281c330822d0fdc2435bf
sabelossa <58365597+sabelossa@users.noreply.github.com>
2022-08-09 22:54:06 +0100

    add api fetch

---
+++ components/PersonFrame.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PersonFrame(props: any) {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default PersonFrame;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,12 @@ import Layout from '../components/Layout';
 import Navbar from '../components/Navbar';
 import Title from '../components/Title';
 import Textbox from '../components/Textbox';
+import { GetServerSideProps } from 'next';
+import PersonFrame from '../components/PersonFrame';
 
 //
 
-const Home: NextPage = () => {
+const Home: NextPage = (data) => {
   return (
     <>
       <Layout title={'Person Viewer'} children={undefined}></Layout>
@@ -20,14 +22,15 @@ const Home: NextPage = () => {
   );
 };
 
-const getStaticProps = async () => {
+export async function getServerSideProps(context: any) {
   const res = await fetch('https://randomuser.me/api/?results=10');
   const data = await res.json();
-  const data2 = data[0];
 
+  //console.log(data);
+  //console.log(data.results);
   return {
-    props: { users: data2 },
+    props: { data }, // will be passed to the page component as props
   };
-};
+}
 
 export default Home;
